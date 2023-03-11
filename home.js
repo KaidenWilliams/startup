655aaed2e53410486b4ec4fef8b65f7f74a9ad33
KaidenWilliams <kman16409@gmail.com>
2023-03-10 22:20:42 -0700

    Background image, much easier if it could be API

---
+++ home.js
@@ -0,0 +1,24 @@
+// Function that adds dot
+
+const map = document.getElementById("mapimage");
+
+map.addEventListener("click", (event) => {
+  const dot = document.createElement("div");
+  dot.classList.add("dot");
+  dot.style.left = event.clientX + "px";
+  dot.style.top = event.clientY + "px";
+  map.appendChild(dot);
+});
+
+
+//Function that creates text box popup, maybe is called by dot function
+
+
+//Function that closes out of current text box also allows you to favorite it, maybe make it a class or object but idk we'll see
+
+
+//Function that allows you to search for activities by category
+
+//Function that allows you to zoom with slider
+
+//Function that allows you to swipe left or right on screen
