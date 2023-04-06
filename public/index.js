138623fc1d1a7e29088fbba29d9312eba4dce5b3
KaidenWilliams <kman16409@gmail.com>
2023-04-05 23:24:09 -0600

    Testing

--- public/index.js
+++ public/index.js
@@ -1,24 +1,24 @@
-const express = require("express")
-const app = express
+// const express = require("express")
+// const app = express
 
-const port = process.argv.length > 2 ? process.argv[2] : 4000;
+// const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
-// Parses Json
-app.use(express.json());
-// Sets up middleware to serve up static files from public directory
-app.use(express.static('public'));
+// // Parses Json
+// app.use(express.json());
+// // Sets up middleware to serve up static files from public directory
+// app.use(express.static('public'));
 
-// Creates new router instance, basically mini express application, used to handle subrequests
-const apiRouter = express.Router();
-// Uses apiRouter to handle all requests that start with /api path, common convention to to distinguish API routes from others in application
-app.use(`/api`, apiRouter)
+// // Creates new router instance, basically mini express application, used to handle subrequests
+// const apiRouter = express.Router();
+// // Uses apiRouter to handle all requests that start with /api path, common convention to to distinguish API routes from others in application
+// app.use(`/api`, apiRouter)
 
 
 
-//Returns Default Page if ever errors
-app.use((_req, res) => {
-    res.sendFile('index.html', { root: 'public' });
-  });
+// //Returns Default Page if ever errors
+// app.use((_req, res) => {
+//     res.sendFile('index.html', { root: 'public' });
+//   });
 
-// Makes it so that express waits for incoming network connections on that port
-app.listen(port, () => {console.log(`Listening on port ${port}`)})
+// // Makes it so that express waits for incoming network connections on that port
+// app.listen(port, () => {console.log(`Listening on port ${port}`)})
