d7d66e646eb7a08b596f5895e65f8a8cb8209fb3
KaidenWilliams <kman16409@gmail.com>
2023-04-05 22:34:42 -0600

    Setting up  Atlas Database

---
+++ database.js
@@ -0,0 +1,17 @@
+const {MongoClient} = require('mongodb');
+require('dotenv').config();
+
+const userName = process.env.MONGOUSER;
+const password = process.env.MONGOPASSWORD;
+const hostname = process.env.MONGOHOSTNAME;
+
+if (!userName) {
+    throw Error('Database not configured. Set environment variables');
+  }
+  
+  const url = `mongodb+srv://${userName}:${password}@${hostname}`;
+  
+  const client = new MongoClient(url);
+  const spotCollection = client.db('Mapz').collection('spot');
+
+  getLocation
--- public/index.js
+++ public/index.js
@@ -13,5 +13,12 @@ const apiRouter = express.Router();
 // Uses apiRouter to handle all requests that start with /api path, common convention to to distinguish API routes from others in application
 app.use(`/api`, apiRouter)
 
+
+
+//Returns Default Page if ever errors
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+  });
+
 // Makes it so that express waits for incoming network connections on that port
 app.listen(port, () => {console.log(`Listening on port ${port}`)})
