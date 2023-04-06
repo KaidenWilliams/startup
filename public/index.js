4682394494fb01588442b0720ff93c79452dcbb5
KaidenWilliams <kman16409@gmail.com>
2023-04-05 21:42:07 -0600

    Moving Files Around to make Service

---
+++ public/index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express
+
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+// Parses Json
+app.use(express.json());
+// Sets up middleware to serve up static files from public directory
+app.use(express.static('public'));
+
+// Creates new router instance, basically mini express application, used to handle subrequests
+const apiRouter = express.Router();
+// Uses apiRouter to handle all requests that start with /api path, common convention to to distinguish API routes from others in application
+app.use(`/api`, apiRouter)
+
+// Makes it so that express waits for incoming network connections on that port
+app.listen(port, () => {console.log(`Listening on port ${port}`)})
